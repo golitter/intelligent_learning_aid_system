9010353340bc3a4266836965c2a99b976b1f2d2a
golitter <1981971524@qq.com>
2024-10-10 09:51:17 +0800

    上传

---
+++ src/main/java/com/intelligent_learning_aid_system/IntelligentLearningAidSystemApplication.java
@@ -0,0 +1,13 @@
+package com.intelligent_learning_aid_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentLearningAidSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentLearningAidSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intelligent_learning_aid_system/IntelligentLearningAidSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligent_learning_aid_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentLearningAidSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
