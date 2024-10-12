062fa5b139ee20b0de217960d1ccef71f46a0681
golitter <1981971524@qq.com>
2024-10-12 18:03:45 +0800

    实现：登录成功，下发JWT令牌

--- src/main/java/com/intelligent_learning_aid_system/controller/LoginController.java
+++ src/main/java/com/intelligent_learning_aid_system/controller/LoginController.java
@@ -3,11 +3,17 @@ package com.intelligent_learning_aid_system.controller;
 import com.intelligent_learning_aid_system.pojo.Emp;
 import com.intelligent_learning_aid_system.pojo.Result;
 import com.intelligent_learning_aid_system.service.EmpService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -19,6 +25,19 @@ public class LoginController {
         log.info("登录成功 {}",emp);
         Emp e = empService.login(emp);
 
-        return e != null ? Result.success(e) : Result.error("用户名或密码错误");
+        // 登录成功，发放令牌
+        if( e != null) {
+            // 生成令牌
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("username", e.getUsername());
+            claims.put("role", "admin");
+            String jwts = Jwts.builder()
+                    .signWith(SignatureAlgorithm.HS256,"gdlm") // 签名算法
+                    .setClaims(claims) // 自定义数据（载荷）
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 设置过期时间
+                    .compact();
+            return Result.success(jwts);
+        }
+        return  Result.error("用户名或密码错误");
     }
 }
--- src/test/java/com/intelligent_learning_aid_system/IntelligentLearningAidSystemApplicationTests.java
+++ src/test/java/com/intelligent_learning_aid_system/IntelligentLearningAidSystemApplicationTests.java
@@ -1,13 +1,38 @@
 package com.intelligent_learning_aid_system;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class IntelligentLearningAidSystemApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testJWT() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", "glm");
+        claims.put("role", "admin");
+        String jwts = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256,"gdlm") // 签名算法
+                .setClaims(claims) // 自定义数据（载荷）
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 设置过期时间
+                .compact();
+        System.out.println(jwts);
+
+        claims = Jwts.parser()
+                .setSigningKey("gdlm")
+                .parseClaimsJws(jwts)
+                .getBody();
+        System.out.println(claims);
     }
 
 }
