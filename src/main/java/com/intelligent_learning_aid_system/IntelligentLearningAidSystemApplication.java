9f501a21f95f67bb0687ff62e50c74943cb1ce24
golitter <1981971524@qq.com>
2024-10-15 14:48:40 +0800

    过滤器部分学习

--- src/main/java/com/intelligent_learning_aid_system/IntelligentLearningAidSystemApplication.java
+++ src/main/java/com/intelligent_learning_aid_system/IntelligentLearningAidSystemApplication.java
@@ -2,7 +2,9 @@ package com.intelligent_learning_aid_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class IntelligentLearningAidSystemApplication {
 
