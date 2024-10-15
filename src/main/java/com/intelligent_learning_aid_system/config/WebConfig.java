cdb08e6e59c38f6e0bd7759b5d1f6cca43f5202d
golitter <1981971524@qq.com>
2024-10-15 15:37:41 +0800

    异常处理springboot

--- src/main/java/com/intelligent_learning_aid_system/config/WebConfig.java
+++ src/main/java/com/intelligent_learning_aid_system/config/WebConfig.java
@@ -8,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    //自定义的拦截器对象
+    //拦截器对象
     @Autowired
     private LoginCheckInterceptor loginCheckInterceptor;
-
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //注册自定义拦截器对象
-        registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/**");//设置拦截器拦截的请求路径（ /** 表示拦截所有请求）
+        registry.addInterceptor(loginCheckInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
     }
 }
